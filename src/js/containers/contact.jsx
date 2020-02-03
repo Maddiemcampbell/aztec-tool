a5fd01ff5034041c1f62cb1c8ea1962709e2264b
Maddie <maddiemcampbell@gmail.com>
2020-02-03 11:11:41 -0800

    adds a contact form and basic styling

--- src/js/containers/contact.jsx
+++ src/js/containers/contact.jsx
@@ -4,8 +4,19 @@ class Contact extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
-                <h4>Contact Page</h4>
+                <h2 className='contact-header'>Contact Aztec Tooling</h2>
+                <form action="https://mailthis.to/maddiecampbelldev@gmail.com"
+                    method="POST" encType="multipart/form-data" className='flex-column'>
+                    <input className='contact-input' type="text" name="name" placeholder="Your name" />
+                    <input className='contact-input' type="email" name="_replyto" placeholder="Your email" />
+                    <textarea className='contact-text-box' name="message" placeholder="Enter your message here"></textarea>
+                    <input className='contact-download' type="file" name="file" placeholder="Attachments (optional)" />
+                    <input className='contact-input' type="hidden" name="_subject" value="Contact form submitted" />
+                    <input className='contact-input' type="hidden" name="_after" value="madelinecampbell.dev" />
+                    <input className='contact-input' type="hidden" name="_honeypot" value="" />
+                    <input className='contact-input' type="hidden" name="_confirmation" value="" />
+                    <button className='contact-button' value="Send">Send</button>
+                </form>
             </div>
         )
     }
