dfee7969d4610660af293d8ce34e4e132a6a6cea
Maddie <maddiemcampbell@gmail.com>
2020-02-10 14:04:16 -0800

    added static styling for services page

--- src/js/containers/services.jsx
+++ src/js/containers/services.jsx
@@ -3,9 +3,55 @@ import React, { Component } from "react";
 class Services extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello World!</h1>
-                <h4>Services Page</h4>
+            <div className="flex-row flex-center margin-top">
+                <div className="services-card">
+                    <h3 className="services-title">Service 1</h3>
+                    <ul className="services-list">
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                    </ul>
+                    <p className="services-description">More description: Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip
+                        ex ea commodo consequat.</p>
+                </div>
+                <div className="services-card">
+                    <h3 className="services-title">Service 2</h3>
+                    <ul className="services-list">
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                    </ul>
+                    <p className="services-description">More description: Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip
+                        ex ea commodo consequat.</p>
+                </div>
+                <div className="services-card">
+                    <h3 className="services-title">Service 3</h3>
+                    <ul className="services-list">
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                        <li className="services-item">Services</li>
+                    </ul>
+                    <p className="services-description">More description: Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip
+                        ex ea commodo consequat.</p>
+                </div>
             </div>
         )
     }
